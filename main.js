6fbcbd72cd4d41d20ea174ee1f421b74d002a8cb
sam22unc <sam22@live.unc.edu>
2021-10-09 13:16:31 -0400

    remove test files

--- main.js
+++
@@ -1,9 +0,0 @@
-import { sumToString } from "./src/mild/mild_1";
-import { getIncreasingArray } from "./src/mild/mild_1";
-import { maxAndMin } from "./src/mild/mild_1";
-import { countArray } from "./src/mild/mild_1";
-
-console.log('sumToString: ' + sumToString(3, 4))
-console.log('getIncreasingArray: ' + getIncreasingArray(3, 7))
-console.dir(maxAndMin([1,2,3,4,5,6,7,8,9]))
-console.log('countArray: ' + countArray([3, 6, 3, 2, 2, 3, 'some', 'hello', 'some', [1, 2]]))
