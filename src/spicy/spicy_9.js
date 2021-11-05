7d384a6e6a615e222d213b3144847f776ec3756f
sam22unc <sam22@live.unc.edu>
2021-11-05 11:29:48 -0400

    so spicy

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -154,7 +154,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x % 2 != 0);
 };
 
 
@@ -164,7 +164,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x % 2 != 0);
 };
 
 
@@ -175,5 +175,5 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    return filter(arr, test).pass.length == n;
 };
